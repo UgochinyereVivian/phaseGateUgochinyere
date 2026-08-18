af02531aa71bd0bcaf24aec8f9f99f39c8282215
Ugochinyere Vivian <your_github_email@example.com>
2026-08-18 15:30:40 +0100

    commit

--- DayTwo/JavaScript/QuestionSix.js
+++ DayTwo/JavaScript/QuestionSix.js
@@ -1,16 +1,41 @@
 
 
-let isPalindrome = (int number) => { 
-        newNumber = Integer.toString(number);
-        reverse = "";
+let runAll = (number) => {
+    if(isPalindrome(number) && isPrime(number)) return true;
+    return false;
+
+}
+
+
+
+let isPalindrome = (number) => { 
+        let newNumber = String(number);
+        let reverse = "";
 
         for(let count = 0; count < array.length; count++){
             reverse = newNumber.charAt(count) + reverse;        
          }
 
-        reverse = Integer.parseInt(reverse);
-        if(reverse == number) return true
+        let newreverse = Number(reverse);
+        if(newreverse == number) return true
     return false
 }
+
+
+
+let isPrime = (number)=> {
+    let count = 0;
+    for(let divisor = 0; divisor <= number; divisor++){
+        if(number % divisor == 0) count++;
+            
+    }
+    if(count == 2) return true;
+
+  return false;
+
+}
+
+
+console.log(runAll(1234));
 
 
