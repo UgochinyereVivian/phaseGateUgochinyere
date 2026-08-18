c9e8eeb587a93d6ed0e3b4b0ea9fc7c3cdf606f5
Ugochinyere Vivian <your_github_email@example.com>
2026-08-18 14:56:56 +0100

    recovered files successfully uploaded

---
+++ DayOne/JavaScript/task_three.js
@@ -0,0 +1,25 @@
+
+
+
+
+function greetWorldOriginal(name){
+        console.log(`
+"My name is "${name}""
+"Today is 17/08/2026"
+`)
+
+}
+
+greetWorldOriginal("ugo")
+
+
+
+
+
+//let greetWorld = (name)=>{
+//        console.log("\"My name is \"" + name + "\"");
+//        console.log("\"Today is 17/08/2026\"");
+//
+//}
+//
+//greetWorld("ugo")
---
+++ DayOne/JavaScript/task_two.js
@@ -0,0 +1,11 @@
+
+
+let greetWorld = ()=>{
+    let count = 0;
+    while(count != 5){
+        console.log('"Hello, World"');
+        count+=1;
+    }
+}
+    
+greetWorld()
